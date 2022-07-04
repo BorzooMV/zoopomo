6bc50cb25a4ec3d0aa31e718d7f54d46a986ca53
Borzoo Moazami <borzoomoazami94@gmail.com>
2022-07-04 20:34:23 +0430

    feat(components): create Box component

---
+++ src/components/common/box/index.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Root = styled.div`
+  background: #fff;
+  padding: 36px 25px;
+  width: fit-content;
+  max-width: 400px;
+  border-radius: 10px;
+`;
+
+export const Title = styled.h2``;
